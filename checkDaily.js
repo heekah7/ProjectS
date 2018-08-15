9333a64883e682cd8974a72ab89bb3b38f01b174
Hee K Ooi <heekah7@gmail.com>
2018-08-15 18:05:46 +0800

    Added Check Daily Data

---
+++ checkDaily.js
@@ -0,0 +1,22 @@
+var mongoose = require( 'mongoose' );
+mongoose.connect( 'mongodb://localhost/TickData' ); // connect to database
+var Counter = require( './model/counter' );
+var Transaction = require( './model/transaction' );
+var DailyChart = require( './model/dailyChart' );
+var array = [];
+Counter.find( {} ).select( 'code -_id' ).lean().exec( function ( err, counter ) {
+    checkBar(counter, counterIndex);
+} );
+
+function checkBar( counter, counterIndex ) {
+    DailyChart.find({code:counter[counterIndex]['code']}).lean().exec(function ( err, dailyData ) {
+        if (dailyData.length == 0){
+            array.push(counter[counterIndex]['code']);
+        }
+        if (counter.length > counterIndex){
+            checkBar(counter, counterIndex);
+        } else {
+            console.log('Done!');
+        }
+    })
+}
