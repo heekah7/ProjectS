813860c2d32b52b9672dc063d36bf25420f7b28c
Hee K Ooi <heekah7@gmail.com>
2018-08-22 15:39:10 +0800

    Added adjust price script

---
+++ adjustPrice.js
@@ -0,0 +1,61 @@
+var mongoose = require( 'mongoose' );
+mongoose.connect( 'mongodb://localhost/TickData' ); // connect to database
+var Counter = require( './model/counter' );
+var Minute1Chart = require( './model/minute1Chart' );
+var stackCall = 0;
+Counter.find( {} ).select( 'code -_id' ).exec( function ( err, counter ) {
+    if ( err ) throw err;
+    // var counterArray = end ? counter.splice( start, end ) : counter.splice( start );
+    getMinuteData( counter, 0 );
+} );
+
+function getMinuteData( counter, counterIndex ) {
+    if (counter.length > counterIndex) {
+        Minute1Chart.find( { code: counter[counterIndex].code }, function ( err, data ) {
+            console.log(data.length);
+            calcRealPrice( counter, counterIndex, data, 0 );
+        } );
+    }
+}
+
+function calcRealPrice( counter, counterIndex, data, dataIndex ) {
+    console.log(dataIndex);
+    if (dataIndex > 1 && ( data[dataIndex].open === 0 && data[dataIndex - 1].close !== 0 )) {
+        stackCall = 0;
+        data[dataIndex].open = data[dataIndex-1].close;
+        data[dataIndex].close = data[dataIndex-1].close;
+        data[dataIndex].low = data[dataIndex-1].close;
+        data[dataIndex].high = data[dataIndex-1].close;
+        data[dataIndex].save(function ( err ) {
+            if (err) throw err;
+            dataIndex += 1;
+            if (dataIndex < data.length) {
+                calcRealPrice(counter, counterIndex, data, dataIndex);
+            } else {
+                getMinuteData( counter, counterIndex + 1);
+            }
+        })
+    } else {
+        stackCall += 1;
+        dataIndex += 1;
+        var timeout = stackCall > 1000 ? 500 : 0;
+
+        if (timeout > 0) {
+            setTimeout(function (  ) {
+                stackCall = 0;
+                if (dataIndex < data.length) {
+                    calcRealPrice(counter, counterIndex, data, dataIndex);
+                } else {
+                    getMinuteData( counter, counterIndex + 1);
+                }
+            }, timeout);
+        } else {
+            if (dataIndex < data.length) {
+                calcRealPrice(counter, counterIndex, data, dataIndex);
+            } else {
+                getMinuteData( counter, counterIndex + 1);
+            }
+        }
+    }
+
+}
